33310ced9aaff9a40254d2a6dd069e25b0d899b2
Frederik <renspand.bornholm@gmail.com>
2026-03-04 19:30:16 +0100

    fix redirect loop

--- src/app/kort/page.tsx
+++ src/app/kort/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import KortClient from "./KortClient";
 
-export default function Home() {
-  redirect("/kort");
+export default function Page() {
+  return <KortClient />;
 }
