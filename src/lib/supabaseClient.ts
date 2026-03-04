ade6833f0965472f293d7b06a103dfec249a161a
Frederik <renspand.bornholm@gmail.com>
2026-03-04 21:08:59 +0100

    fix supabase url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://oqwofgamwudwgbpgxsf.supabase.co";
-
 const supabaseAnonKey =
   "sb_publishable_lRYvoYHv9HTd-JgUgnJzEg_Yk6zpyAj";
 
